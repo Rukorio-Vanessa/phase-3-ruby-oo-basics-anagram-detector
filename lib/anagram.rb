106df03e298c908df250fbdad13a5a9fcfeb054f
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-02-13 22:29:15 +0300

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+  
+    def initialize (word)
+      @word = word
+    end
+  
+    def match list
+      arr_of_matches = []
+      list.each do |item|
+        if item.chars.sort == @word.chars.sort
+          arr_of_matches.push(item)
+        end
+      end
+
+      return arr_of_matches 
+
+    end  
+
+  end  
+ binding.pry
